123dbe204e8a10c350eab2d3e599cfbd513f33c9
Kelvin Marques <Kelvin.cordeiro.ext@nokia.com>
2021-06-28 14:10:28 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Scraping-wikipedia-mentorama-pro
+Projeto para extrair os titulos de todos links dentro do texto de um tema do wikipedia. 
---
+++ spider.py
@@ -0,0 +1,25 @@
+
+import re
+import html5lib
+from requests import get
+from bs4 import BeautifulSoup
+
+titulos= set()
+
+resposta = get("https://pt.wikipedia.org/wiki/Pain_Gaming")
+tags = BeautifulSoup(resposta.text, "html5lib")
+
+title = tags.find("title")
+print(title)
+
+p = tags.find_all("p")
+
+for i in p:
+    anchors = i.find_all("a", href=re.compile("/wiki/"))
+    for a in anchors:
+       
+        titulos.add(a.get('title'))
+
+
+print(titulos)
+   
